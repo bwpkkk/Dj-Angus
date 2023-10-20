d9272cfddb198598bb3f956d804afe875e145b55
妇科圣手bwp <82914235+bwpkkk@users.noreply.github.com>
2023-10-20 17:56:20 -0500

    Add files via upload

--- Reg/forms.py
+++ Reg/forms.py
@@ -1,11 +1,25 @@
 from django.forms import ModelForm
 from .models import User
+from django import forms
 class UserFrom(ModelForm):
     class Meta:
         model=User
-        fields=["DateofBirth","profile_picture"]
+        fields=["DateofBirth","profile_picture","personal_Bio"]
        # labels=['DateofBirth','profile_picture']
         error_messages = {
-        " DateofBirth": {'required': '用户名不能为空'},
-        " profile_picture": {'required': '用户名不能为空'},
+        " DateofBirth": {'required': "Date of Birth can't be empty"},
+        " profile_picture": {'required': "profile picture can't be empty"},
     }
+        widgets={
+            "DateofBirth":forms.DateInput(attrs={"class":"form-control",}),
+            "profile_picture":forms.FileInput(attrs={"class":"form-control"}),
+            "personal_Bio":forms.Textarea(attrs={"class":"form-control", })
+            #"placeholder":
+        }
+
+
+        def __init___(self,*args,**kwargs):
+            super().__init__(*args,**kwargs)
+
+            for name,field in self.fields.items():
+                print (name,field)
--- Reg/models.py
+++ Reg/models.py
@@ -10,13 +10,13 @@ class User(models.Model):
     DateofBirth = models.DateField("Date of Birth",auto_now_add=False,blank=True,null=True)
     isStudent=models.BooleanField(default=False)
     profile_picture=models.ImageField("Profile Picture",upload_to='media/profile_pictures',blank=True,null=True)
-    personal_Bio=models.CharField("Bio", max_length=200 )
+    personal_Bio=models.CharField("Bio", max_length=200,blank=True, null=True )
 
     class meta:
         abstract = True
 
-   # def __str__(self):
-       # return 'Username %s'(self.username)
+    def __str__(self):
+        return self.username
 
 
 
@@ -26,28 +26,47 @@ class Teacher(User):
     personal_rate=models.DecimalField("rate",default=0, decimal_places=2,max_digits=6)
     numberofSudents = models.IntegerField(default=0)
 
-
-class Student(User):
-    Courses_bought = models.IntegerField(default=0)
-    Quetions_asked=models.IntegerField(default=0)
-    Maximum_questions=models.IntegerField(default=3)
+    def __str__(self):
+       return self.username
 
 
 class Course(models.Model):
-    course_name=models.CharField("Course Name", max_length=30, unique=True)
-    course_description=models.CharField("Course Description", max_length=100,blank=True)
+    course_name = models.CharField("Course Name", max_length=30, unique=True)
+    course_description = models.CharField("Course Description", max_length=100, blank=True)
     created_time = models.DateTimeField('Created time', auto_now_add=True)
-    owner= models.ForeignKey(Teacher,on_delete=models.CASCADE,default=1)
-    numberofSudentsBought=models.IntegerField(default=0)
+    owner = models.ForeignKey(Teacher, on_delete=models.CASCADE, default=1)
+
     price = models.DecimalField("Price", default=0, decimal_places=2, max_digits=6)
     course_picture = models.ImageField("Course Picture", upload_to='media/course_pictures', blank=True, null=True)
 
+    def __str__(self):
+        return self.course_name
+
+
+class Student(User):
+    Courses_bought = models.IntegerField("Course bought",default=0)
+    Quetions_asked=models.IntegerField("Quetions_asked",default=0)
+    Maximum_questions=models.IntegerField("Maximum_questions",default=3)
+    Teacher = models.ManyToManyField(Teacher,blank=True, null=True)
+    Courses = models.ManyToManyField(verbose_name="Courses",to=Course)
+
+    def __str__(self):
+       #return 'Student %s'(self.username)
+       return self.username
+
+
+
+
 
 class Question(models.Model):
-    course_name=models.CharField("Question Name", max_length=30, unique=True)
-    course_description=models.CharField("Question Description", max_length=100)
+
+    question_Name=models.CharField("Question Name", max_length=30, unique=True)
+    question_Description=models.CharField("Question Description", max_length=100)
     created_time = models.DateTimeField('Created time', auto_now_add=True)
     owner=models.ForeignKey(Student,on_delete=models.CASCADE,default=1)
     isSolved=models.BooleanField('isSolved',default=False)
-    solvedby=   models.ForeignKey(Teacher,on_delete=models.CASCADE,default=1)
+    solvedby= models.ForeignKey(Teacher,on_delete=models.CASCADE,default=1)
 
+
+    def __str__(self):
+        return self.question_Name
