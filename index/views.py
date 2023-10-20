34ce5cfae48a9663bbc507d2318fb7577fafe640
妇科圣手bwp <82914235+bwpkkk@users.noreply.github.com>
2023-10-20 17:53:34 -0500

    Add files via upload

--- index/views.py
+++ index/views.py
@@ -1,15 +1,21 @@
 from django.shortcuts import render
-from Reg.models import User,Teacher,Student
+from Reg.models import User,Teacher,Student,Course,Question
 
 def index_view(request):
     try:
         user = User.objects.get(username=request.session['username'])
         teachers = Teacher.objects.all()
         img = user.profile_picture
+        numofCourses= Course.objects.all().count()
 
-        print(user. profile_picture)
+        Courses= Course.objects.all()
+        course=Course.objects.get(course_name="Fancy gun")
+        students=course.student_set.all()
+        #student=Student.objects.filter(username="Mad Max").first()
 
-        return render(request, 'index/index.html', {'img': img, 'teachers': teachers})
+
+        print(numofCourses)
+        return render(request, 'index/index.html', {'img': img, 'teachers': teachers,'courses': Courses})
     except Exception as e:
         print('--login user error 2 %s' % (e))
         return render(request, 'index/index.html')
